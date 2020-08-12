4c0fe96ae3a0de672ccabab998df573e564d66db
huangkeke28 <huangkeup@163.com>
2020-08-12 23:31:00 +0800

    SpringBoot初始

--- spring-study/src/main/java/Main.java
+++ spring-study/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import frank.Duck3;
+import frank.DuckShop;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,12 +12,24 @@ public class Main {
         ApplicationContext context = new
                 ClassPathXmlApplicationContext("applications.xml");
        //通过bean的名称获取bean对象,bean的名称就是xml中bean的id
-        String bit = (String)context.getBean("bit");
+        //String bit = (String)context.getBean("bit");
        // String bit2 = (String)context.getBean("string");
-        System.out.println(bit);
+       // System.out.println(bit);
        // System.out.println(bit2);
 
         //通过类型获取bean对象,如果该类型有多个对象就会报错,只支持一个该类型的对象
-        context.getBean(String.class);
+        //context.getBean(String.class);
+
+        Duck3 duck1 = (Duck3)context.getBean("duck1");
+        System.out.println(duck1);
+
+        DuckShop ds = (DuckShop)context.getBean("shop");
+        System.out.println(ds);
+        //Spring Boot
+        //作用:减少配置性的工作
+        //如web项目传统开发步骤
+        //(1)创建web项目结构,包括web资源文件夹和web.xml
+        //(2)使用spring FrameWork配置很多xml文件 文件中配置很多内容
+        //(3)部署到tomcat
     }
 }
